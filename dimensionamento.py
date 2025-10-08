704a29c1b1850adaa62042ae00d003168453598e
Diogo Marinho <diogomarinho7090@gmail.com>
2025-10-08 11:45:35 -0400

    etapa inicial

--- dimensionamento.py
+++ dimensionamento.py
@@ -1,13 +1,15 @@
 #Dimensionamento da Casa Flutuante
 
 class Dimensionamento:
-    def __init__(self, pesos = []):
+    def __init__(self, pesos = [], volume_concreto = 0.0):
         self.pesos = pesos #kg
+        self.volume_concreto = volume_concreto
 
-
+        #Constantes
         self.DENSIDADE_AGUA = 1000  # kg/m³
         self.GRAVIDADE = 9.81 #m/s²
-        self.PESO_ESPECIFICO_AGUA = 10e3 # N/m³
+        self.PESO_ESPECIFICO_AGUA = 10 # KN/m³
+        self.PESO_ESPECIFICO_CONCRETO = 25  # kN/m³
 
     def volume_agua_deslocado(self):
 
@@ -24,3 +26,6 @@ class Dimensionamento:
         ql = hl * self.PESO_ESPECIFICO_AGUA
 
     def carga_peso(self):
+        peso = self.PESO_ESPECIFICO_CONCRETO * self.volume_concreto
+
+        return peso
